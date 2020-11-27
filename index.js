f7be35016ff34a9ec95d4de2302e12b954a80c06
Pablo Gonzalez <pabgon0@gmail.com>
2020-11-27 01:46:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function writeCards(names, event) {
   let cards_array = []
-  for (i = 0; i < names.length; i++) {
+  for (let i = 0; i < names.length; i++) {
     cards_array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
   }
   return cards_array;
